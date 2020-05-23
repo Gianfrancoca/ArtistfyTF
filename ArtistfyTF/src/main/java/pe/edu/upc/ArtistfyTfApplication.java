ea1a6d8529662faeb0c890d6b04c1326f35b94e9
Gianfranco Cervera <u201523986@upc.edu.pe>
2020-05-22 21:30:19 -0500

    First commit

---
+++ ArtistfyTF/src/main/java/pe/edu/upc/ArtistfyTfApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistfyTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistfyTfApplication.class, args);
+	}
+
+}
