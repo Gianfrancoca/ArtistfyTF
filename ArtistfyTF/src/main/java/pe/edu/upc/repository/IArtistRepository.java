2ab10c0128b469a2782b29dc0e4028a2cf30ec3f
CalitoFC <64799081+CalitoFC@users.noreply.github.com>
2020-06-06 01:28:47 -0500

    Create IArtistRepository.java
    Repositorio 1

---
+++ ArtistfyTF/src/main/java/pe/edu/upc/repository/IArtistRepository.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.entity.Artist;
+
+@Repository
+public interface IArtistRepository extends JpaRepository<Artist, Integer>{
+
+	@Query("select count(a.dni) from Artist a where a.dni=:dni")
+	public int searchDni(@Param("dni") int documento);
+}
