ac99389ebc0db21e47754d0078a555e468cc891c
Gerson2803 <64698109+Gerson2803@users.noreply.github.com>
2020-06-20 00:08:02 -0500

    IArtistService

---
+++ ArtistfyTF/src/main/java/pe/edu/upc/service/IArtistService.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import pe.edu.upc.entity.Artist;
+
+public interface IArtistService {
+
+	public int insert(Artist artist);
+	List<Artist> listArtist();
+	public void delete(int idArtist);
+	Optional<Artist> searchId(int idArtist);
+}
