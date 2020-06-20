17cfb39433ed15a51ecadcc796a15ead10fc85a5
Gerson2803 <64698109+Gerson2803@users.noreply.github.com>
2020-06-20 00:00:10 -0500

    IEventRepository

---
+++ ArtistfyTF/src/main/java/pe/edu/upc/repository/IEventRepository.java
@@ -0,0 +1,12 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.entity.Event;
+
+
+
+@Repository
+public interface IEventRepository extends JpaRepository<Event, Integer>{
+
+
+}
